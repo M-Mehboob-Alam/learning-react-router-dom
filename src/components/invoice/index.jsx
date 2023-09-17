6041b2d76d5eecbbfc21449a8eae9c3dc282f717
Mehboob Alam <mehboobalam.dev@gmail.com>
2023-09-17 08:15:55 +0500

    learn to add Current Active link via NavLink

--- src/components/invoice/index.jsx
+++ src/components/invoice/index.jsx
@@ -1,19 +1,33 @@
-import { Link, Outlet } from "react-router-dom";
+import { Link, NavLink, Outlet } from "react-router-dom";
 import { getInvoices } from "../data";
 export default function invoice(){
     let  invoices = getInvoices();
     return (
         <main className="text-center">
             <Outlet></Outlet>
-                <div className="flex flex-col gap-3">
+                <div className="flex flex-col gap-3 mt-5">
                     {invoices.map(invoice =>(
-                        <Link 
+                        <NavLink 
                         to={`/invoice/${invoice.name}`}
                         key={invoice.name}
-                        
-                        className="bg-gray-200">
+                        style={({isActive})=> {
+                            return {
+                                color : isActive ? 'white' : '',
+                                backgroundColor : isActive ? 'black': ''
+                            
+                            }
+                        }}
+                        className="bg-gray-200"
+                        >
                             {invoice.name}
-                        </Link>
+                        </NavLink>
+                        // <Link 
+                        // to={`/invoice/${invoice.name}`}
+                        // key={invoice.name}
+                        
+                        // className="bg-gray-200">
+                        //     {invoice.name}
+                        // </Link>
                     ))}
                 </div>
                 
